385f6bdf43fddd5755212dc429d6be075ae048f0
airoker80 <kazekage556@gmail.com>
2017-05-01 16:04:27 +0545

    Added p2p chat

---
+++ app/src/main/java/com/example/sameer/myapplication/Wifip2p/WiFiP2pService.java
@@ -0,0 +1,13 @@
+package com.example.sameer.myapplication.Wifip2p;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by Sameer on 5/1/2017.
+ */
+
+public class WiFiP2pService {
+    WifiP2pDevice device;
+    String instanceName = null;
+    String serviceRegistrationType = null;
+}
